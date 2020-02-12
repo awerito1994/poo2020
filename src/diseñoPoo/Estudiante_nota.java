47c6ad39a0afd2a9f9331e12e606c1e32d3918e6
awerito1994 <alex_awuero@hotmail.com>
2020-02-12 00:12:39 -0500

    Create Estudiante_nota.java

---
+++ src/diseñoPoo/Estudiante_nota.java
@@ -0,0 +1,50 @@
+package diseñoPoo;
+
+public class Estudiante_nota {
+    private String nombre;
+    private String materia;
+    private double primer_bimestre;
+    private double segundo_bimestre;
+
+    //constructor
+
+    public Estudiante_nota(String nombre, String materia, double primer_bimestre, double segundo_bimestre) {
+        this.nombre = nombre;
+        this.materia = materia;
+        this.primer_bimestre = primer_bimestre;
+        this.segundo_bimestre = segundo_bimestre
+    }
+
+    /**
+     * Método para obtener las notas de los 2 bimestres
+     *
+     * @param primer_bimestre
+     * @param segundo_bimestre
+     * @return
+     */
+    public String obtener_calificacion(double primer_bimestre, double segundo_bimestre) {
+
+        String calificacion;
+        double suma= primer_bimestre + segundo_bimestre;
+        if (suma >= 28) {
+            calificacion = "Su nota es aprobada, no debe rendir supletorio";
+        } else {
+            calificacion = "Su nota no es aprobaba, debe  rendir supletorio";
+        }
+        return calificacion;
+
+    }
+
+    /**
+     * Método para mostrar la nota del estudiante
+     * @return
+     */
+    public String presentacion(){
+        String presentacion = String.format("El alumno: %s\nEn la materia: %s\n" +
+                        "Con las notas:\n1er Bim: %.2f\n2do Bim: %.2f\nNota Final: %.2f \n%s"
+                , nombre, materia, primer_bimestre, segundo_bimestre, primer_bimestre + segundo_bimestre, obtener_calificacion(this.primer_bimestre, this.segundo_bimestre));
+        return presentacion;
+
+    }
+}
+
