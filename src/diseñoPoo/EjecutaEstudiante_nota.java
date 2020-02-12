86764951989449a4f7a1d52cb5b8da7f350b23f3
awerito1994 <51511614+awerito1994@users.noreply.github.com>
2020-02-12 00:16:17 -0500

    Add files via upload

---
+++ src/diseñoPoo/EjecutaEstudiante_nota.java
@@ -0,0 +1,11 @@
+package diseñoPoo;
+
+public class EjecutaEstudiante_nota {
+    public static void main(String[] args) {
+        //variable
+        String nombre;
+        String materia;
+        double primer_bimestre;
+        double segundo_bimestre;
+    }
+}
