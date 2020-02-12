d98abe9f8a7cae17a2f50f18710b443b092eeb9e
awerito1994 <51511614+awerito1994@users.noreply.github.com>
2020-02-12 00:18:36 -0500

    Add files via upload

---
+++ src/diseñoPoo/EjecutaEstudiante_nota.java
@@ -0,0 +1,31 @@
+package diseñoPoo;
+import Taller_uno.Estudiante;
+
+import java.util.Scanner;
+
+public class EjecutaEstudiante_nota {
+    public static void main(String[] args) {
+        //variable
+        String nombre;
+        String materia;
+        double primer_bimestre;
+        double segundo_bimestre;
+
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingrese el nombre del estudiante: ");
+        nombre = teclado.nextLine();
+        System.out.print("Ingrese la materia del estudiante: ");
+        materia = teclado.nextLine();
+        System.out.print("Ingrese la nota del primer bimestre: ");
+        primer_bimestre = teclado.nextDouble();
+        System.out.print("Ingrese la nota del segundo bimestre: ");
+        segundo_bimestre = teclado.nextDouble();
+
+
+
+        //creamos el objeto
+        Estudiante_nota estudiante = new Estudiante_nota(nombre, materia,primer_bimestre,segundo_bimestre);
+        System.out.println(estudiante.presentacion());
+    }
+}
