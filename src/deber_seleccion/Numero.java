aafece79daefc2676c7c2b62442f08d90c15c2d4
awerito1994 <alex_awuero@hotmail.com>
2020-02-13 23:58:00 -0500

    Ingreso de comentario en los metodos

--- src/deber_seleccion/Numero.java
+++ src/deber_seleccion/Numero.java
@@ -7,13 +7,23 @@ public class Numero {
     private double numC;
     private double numD;
     private double numMayor;
-
+    /**
+     * Método constructor para los números
+     * @param numA
+     * @param numB
+     * @param numC
+     * @param numD 
+     */
     public Numero(double numA,double numB,double numC,double numD){
         this.numA = numA;
         this.numB = numB;
         this.numC = numC;
         this.numD = numD;
     }
+    /**
+     * ´Método para calcular cual de los números es mayor
+     * @return 
+     */
    public double calcularNumMayor(){
        if(numA > numB && numA >numC && numA >numD){
            numMayor = numA;
@@ -26,6 +36,10 @@ public class Numero {
        }
        return numMayor;
    }
+   /**
+    * Método para mostrar en la impresión el numero mayor
+    * @return 
+    */
    public String mostrar(){
           String mostrar_numero=String.format("El nÃºmero mayor es: %.2f",calcularNumMayor());
           return mostrar_numero;
